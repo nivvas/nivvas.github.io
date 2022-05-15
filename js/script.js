da38e1fd2bb05fb3f5449932e0e4b7d9e7ae223a
nivvas <deden3654@gmail.com>
2022-05-16 02:07:54 +0400

    video was changed

--- js/script.js
+++ js/script.js
@@ -157,43 +157,6 @@ video.addEventListener('ended', () => {
 });
 
 
-// let c = document.getElementById("c");
-// let ctx = c.getContext("2d");
-
-// c.height = window.innerHeight;
-// c.width = window.innerWidth;
-
-// let chinese = "田由甲申甴电甶男甸甹町画甼甽甾甿畀畁畂畃畄畅畆畇畈畉畊畋界畍畎畏畐畑";
-// chinese = chinese.split("");
-
-// let font_size = 10;
-// let columns = c.width/font_size;
-// let drops = [];
-// for(let x = 0; x < columns; x++)
-// 	drops[x] = 1; 
-
-// function draw()
-// {
-// 	ctx.fillStyle = "rgba(0, 0, 0, 0.05)";
-// 	ctx.fillRect(0, 0, c.width, c.height);
-	
-// 	ctx.fillStyle = "rgba(163, 99, 223, 0.3)";
-// 	ctx.font = font_size + "px arial";
-// 	for(let i = 0; i < drops.length; i++)
-// 	{
-
-// 		let text = chinese[Math.floor(Math.random()*chinese.length)];
-// 		ctx.fillText(text, i*font_size, drops[i]*font_size);
-		
-// 		if(drops[i]*font_size > c.height && Math.random() > 0.975)
-// 			drops[i] = 0;
-		
-// 		//incrementing Y coordinate
-// 		drops[i]++;
-// 	}
-// }
-
-// setInterval(draw, 60);
 
 
 
